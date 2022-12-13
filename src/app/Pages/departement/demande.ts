28ead5f196287c913693297048f505acaa8ddc83
BaccarSabri2 <115673916+BaccarSabri2@users.noreply.github.com>
2022-12-14 00:03:50 +0100

    commit
    commit

---
+++ src/app/Pages/departement/demande.ts
@@ -0,0 +1,12 @@
+import { Etudiant } from "src/app/etudiant";
+import { Departements } from "./departements";
+
+export class Demande {
+    departement:Departements
+    statut:string
+    date:Date
+    idDemande:number
+    etudiants:Etudiant
+
+
+}
