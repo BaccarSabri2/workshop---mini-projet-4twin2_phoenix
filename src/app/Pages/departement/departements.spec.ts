06d7fe8291cf954ee508ad553fbcebe6658c8e97
BaccarSabri2 <115673916+BaccarSabri2@users.noreply.github.com>
2022-12-14 00:01:22 +0100

    commit
    commit

--- src/app/Pages/departement/departements.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Departements } from './departements';
-
-describe('Departements', () => {
-  it('should create an instance', () => {
-    expect(new Departements()).toBeTruthy();
-  });
-});
--- src/app/etudiant.ts
+++
@@ -1,7 +0,0 @@
-export class Etudiant {
-    prenomE!: string;
-    nomE!: string;
-    option:string  | undefined
-    idEtudiant:number | undefined
-
-}
